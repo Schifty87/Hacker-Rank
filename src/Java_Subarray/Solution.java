33515094dfee1ff08a77a39acffcce74d78c5d1b
Schifty87 <ryan.j.schiferl@gmail.com>
2017-06-07 16:54:16 -0500

    new code

--- src/Java_Subarray/Solution.java
+++ src/Java_Subarray/Solution.java
@@ -6,9 +6,37 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
+
+
 public class Solution {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    	Scanner s = new Scanner(System.in);
+        int num = s.nextInt();
+        int[] arr = new int[num];
+        for(int i=0; i<num; i++){
+        	arr[i] = s.nextInt();
+        }
+              
+        ArrayList<Integer> sum = new ArrayList<Integer>();
+        
+        //initialize negative number tally
+        int neg = 0;
+        
+        for(int i=0; i<arr.length; i++){
+        	sum.add(arr[i]);
+        	int total = arr[i];
+        	for(int j=i+1; j<arr.length; j++){
+        		total += arr[j];
+        		sum.add(total);
+        	}
+        }
+        int total = 0;
+        for(int i=0; i<sum.size(); i++){
+        	if(sum.get(i)<0){
+        		total++;
+        	}
+        }
+        System.out.println(total);
     }
 }
