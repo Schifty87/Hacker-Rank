e4aeedeea403db385dce90ad44be0a77dfc83bde
Schifty87 <ryan.j.schiferl@gmail.com>
2017-06-07 23:01:32 -0500

    More HackerRank stuff

---
+++ src/Java_Arraylist/Solution.java
@@ -0,0 +1,40 @@
+package Java_Arraylist;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args){
+		//initialize parent arraylist
+		ArrayList <ArrayList<Integer>> arr = new ArrayList<ArrayList<Integer>>();
+		Scanner S = new Scanner(System.in);
+		int N= S.nextInt();
+		
+		for(int i=0; i<N; i++){
+			//find the number of integers to be read in
+			int num = S.nextInt();
+			//initialize child ArrayList
+			ArrayList<Integer> m = new ArrayList<Integer>();
+			for(int j=0; j<num; j++){
+				m.add(S.nextInt());
+			}
+			//add child arraylist to parent
+			arr.add(m);			
+		}
+		
+		//find the number of searches
+		int H = S.nextInt();
+		
+		for(int i=0; i<H; i++){
+			int x = S.nextInt();
+			int y = S.nextInt();
+			ArrayList<Integer> W = arr.get(x-1);
+			if(x>arr.size()||y>arr.get(x-1).size()){
+				System.out.println("ERROR!");
+			}
+			else{
+				System.out.println(arr.get(x-1).get(y-1));
+			}
+		}	
+	}
+}
