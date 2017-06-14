7a59ab626f9cf60a9d62aa46ac7e83f7a5dfb61d
Schifty87 <ryan.j.schiferl@gmail.com>
2017-06-14 18:03:55 -0500

    new hacker rank stuff

---
+++ src/Java_Generics/Printer.java
@@ -0,0 +1,13 @@
+package Java_Generics;
+
+public class Printer {
+	
+	public Printer(){}
+	
+	public void printArray(Object[] a){
+		for(int i=0; i<a.length; i++){
+			System.out.println(a[i]);
+		}
+	}
+
+}
---
+++ src/Java_Hashset/Solution.java
@@ -0,0 +1,26 @@
+package Java_Hashset;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+	public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+        String [] pair_left = new String[t];
+        String [] pair_right = new String[t];
+        
+        for (int i = 0; i < t; i++) {
+            pair_left[i] = s.next();
+            pair_right[i] = s.next();
+        }
+        HashSet<String> n = new HashSet<String>();
+        for (int i=0; i<t; i++){
+        	n.add(pair_left[i] + " " + pair_right[i]);
+        	System.out.println(n.size());
+        }
+	}
+}
